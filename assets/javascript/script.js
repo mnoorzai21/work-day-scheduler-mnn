4350d2414f61c5995bab27df915d5c852e0a0e4e
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2022-04-06 20:05:12 -0700

    update pseudocode and  created js file get ready

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+// header to show current date with time 
+// column of hours from 9 - 5 pm
+// column of descriptions to hold client data
+// column of save to save data
+// past current an future colors
+// save in the local storage
+// after refreshing the data still be saved
